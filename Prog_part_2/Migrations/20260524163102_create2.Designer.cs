edc1f1501819f5d330cfa4c80a096af418db865a
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-05-24 18:54:17 +0200

    commit 3

--- Prog_part_2/Migrations/20260524163102_create2.Designer.cs
+++ Prog_part_2/Migrations/20260524163102_create2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Prog_part_2.Data;
 
@@ -11,9 +12,11 @@ using Prog_part_2.Data;
 namespace Prog_part_2.Migrations
 {
     [DbContext(typeof(Prog_part_2Context))]
-    partial class Prog_part_2ContextModelSnapshot : ModelSnapshot
+    [Migration("20260524163102_create2")]
+    partial class create2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Prog_part_2/Migrations/20260524163102_create2.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prog_part_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class create2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContractFiles_Contracts_ContractsId",
+                table: "ContractFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ContractFiles_ContractsId",
+                table: "ContractFiles");
+
+            migrationBuilder.DropColumn(
+                name: "ContractsId",
+                table: "ContractFiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContractFiles_ContractId",
+                table: "ContractFiles",
+                column: "ContractId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContractFiles_Contracts_ContractId",
+                table: "ContractFiles",
+                column: "ContractId",
+                principalTable: "Contracts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContractFiles_Contracts_ContractId",
+                table: "ContractFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ContractFiles_ContractId",
+                table: "ContractFiles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContractsId",
+                table: "ContractFiles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContractFiles_ContractsId",
+                table: "ContractFiles",
+                column: "ContractsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContractFiles_Contracts_ContractsId",
+                table: "ContractFiles",
+                column: "ContractsId",
+                principalTable: "Contracts",
+                principalColumn: "Id");
+        }
+    }
+}
