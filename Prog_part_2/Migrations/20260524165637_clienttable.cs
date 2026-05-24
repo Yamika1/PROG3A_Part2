8073c253c9896e54bab1d15aced0521c831121d6
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-05-24 19:19:20 +0200

    commit 5

---
+++ Prog_part_2/Migrations/20260524165637_clienttable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prog_part_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class clienttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
